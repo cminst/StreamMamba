e844654ec1b9dad4f5f4d3ce4d5e207ddaef256a
Chungust <qingy2019@outlook.com>
2025-07-03 10:45:58 -0700

    Added script to generate sampler states (sh

---
+++ InternVideo2/multi_modality/scripts/pretraining/clip/B14/generate_sampler_state.sh
@@ -0,0 +1,25 @@
+echo "PYTHONPATH: ${PYTHONPATH}"
+which_python=$(which python)
+echo "which python: ${which_python}"
+export PYTHONPATH=${PYTHONPATH}:${which_python}
+export PYTHONPATH=${PYTHONPATH}:.
+echo "PYTHONPATH: ${PYTHONPATH}"
+
+JOB_NAME='B14'
+OUTPUT_DIR="$(dirname $0)/$JOB_NAME"
+LOG_DIR="./logs/${JOB_NAME}"
+PARTITION='video'
+NNODE=1
+NUM_GPUS=1
+NUM_CPU=64
+
+# Using torchrun directly instead of the wrapper script
+torchrun \
+    --nnodes=${NNODE} \
+    --nproc_per_node=${NUM_GPUS} \
+    --rdzv_backend=c10d \
+    generate_sampler_state.py \
+    $(dirname $0)/config.py \
+    output_dir ${OUTPUT_DIR} \
+    --global_step 2200 \
+    --output_path sampler_states_2200.pkl
