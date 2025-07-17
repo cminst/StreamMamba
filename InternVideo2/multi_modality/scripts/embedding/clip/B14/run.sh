7dd5892fa20aab389bad4b5f620cacc513445f85
Chungust <qingy2019@outlook.com>
2025-07-17 11:02:56 -0700

    update run.sh for embedding script so that it uses torchrun

--- InternVideo2/multi_modality/scripts/embedding/clip/B14/run.sh
+++ InternVideo2/multi_modality/scripts/embedding/clip/B14/run.sh
@@ -9,8 +9,14 @@ JOB_NAME='B14_embed'
 OUTPUT_DIR="$(dirname $0)/$JOB_NAME"
 LOG_DIR="./logs/${JOB_NAME}"
 PARTITION='video'
+NNODE=1
+NUM_GPUS=4
+NUM_CPU=100
 
-python3 \
-    tasks_clip/precompute_embeddings.py \
+torchrun \
+    --nnodes=${NNODE} \
+    --nproc_per_node=${NUM_GPUS} \
+    --rdzv_backend=c10d \
+    tasks_clip/train_spfs.py \
     $(dirname $0)/config.py \
     output_dir ${OUTPUT_DIR}
